c8e72e82df1eb23bd4ea477a7d9a26ebf5ad3dba
Rohit <rohitps008gmail.com>
2026-04-15 16:47:20 +0530

    updated the Dummy class as per the review comments

--- src/main/java/Practice_Dummy/DummyClass.java
+++ src/main/java/Practice_Dummy/DummyClass.java
@@ -1,14 +1,16 @@
 package Practice_Dummy;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class DummyClass {
 	WebDriver driver;
-public WebElement waitforElement(By locator)
+public List<WebElement> waitforElement(By locator)
 {
-	return driver.findElement(locator);
+	return driver.findElements(locator);
 }
 	
 }
