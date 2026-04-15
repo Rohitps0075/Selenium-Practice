ef7244a1f347a1ea4aa027cc6c40fa939c987385
Rohitps0075 <94100840+Rohitps0075@users.noreply.github.com>
2026-04-15 18:10:03 +0530

    Create stash.java

---
+++ src/main/java/Practice/stash.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class stash {
+
+	String name="stash_Rohit";
+	
+	public void checkStashScenario()
+	{
+		System.out.println("let's check stash-Rohit scenario");
+	}
+	
+	
+}
